446045b5409550b9f46cfee2c146e424b7010209
CinCoutTechnology <53955898+CinCoutTechnology@users.noreply.github.com>
2024-07-23 18:22:05 -0500

    update db user

--- database/index.ts
+++ database/index.ts
@@ -3,4 +3,3 @@ export * as dbOrders from './dbOrders';
 export * as dbCola from './dbCola';
 export * as dbReserva from './dbReserva';
 export * as dbUsers from './dbUsers';
-export * as seedDatabase from './seed-data';
