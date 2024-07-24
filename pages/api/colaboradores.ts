e3889ed8eea271c0d91bb0c26ddef08ef8d8f69f
CinCoutTechnology <53955898+CinCoutTechnology@users.noreply.github.com>
2024-07-24 11:54:41 -0500

    debug horas

--- pages/api/colaboradores.ts
+++ pages/api/colaboradores.ts
@@ -79,6 +79,7 @@ const getColaborador = async (req: NextApiRequest, res: NextApiResponse) => {
         }
 
         const fechaSelect = new Date(`${data.selectedDate}`);
+        console.log(`Fecha recivida ${fechaSelect}`);
 
         const fechaLimite = new Date();
         fechaLimite.setDate(mindata().getDate() + 21);
@@ -99,8 +100,6 @@ const getColaborador = async (req: NextApiRequest, res: NextApiResponse) => {
             category: categoria,
             state: true
         }).lean();
-
-        console.log(colaboradores)
 
         await db.disconnect();
 
@@ -161,17 +160,23 @@ const getColaborador = async (req: NextApiRequest, res: NextApiResponse) => {
             }
         });
 
-
-        console.log(colaboradores);
+        console.log(`Fechas en numeros - resivida: ${fechaSelect}`)
+        console.log(`Fechas en numeros - sistema: ${mindata()}`)
 
         if (startOfDay(fechaSelect).toISOString() === startOfDay(mindata()).toISOString()) {
+            console.log('entro')
+            console.log(`Fecha del sistema: ${mindata()}`)
             const nowTime = mindata().getHours() * 60 + mindata().getMinutes();
+
+            console.log(`Nueva hora: ${nowTime}`)
             const thresholdMinutes = 60;
             const thresholdTime = nowTime + thresholdMinutes;
+            console.log(`Hora limite: ${thresholdTime}`)
 
             const filteredShifts = result.filter(shift => {
                 const shiftTime = parseTimeToDate(shift.hour);
                 const shiftMinutes = shiftTime.getHours() * 60 + shiftTime.getMinutes();
+                console.log(`Hora seleccionada: ${shiftMinutes}`)
                 return shiftMinutes >= thresholdTime;
             });
 
